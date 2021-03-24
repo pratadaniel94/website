73e6c7011282049c8ee7ca79a962bf1483558425
pratadaniel94 <danielprata@outlook.com>
2021-03-24 05:30:27 -0300

    init layout project

--- Website/urls.py
+++ Website/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from core.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', index, name="index")
 ]
